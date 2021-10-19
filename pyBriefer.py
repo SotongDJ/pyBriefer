84e7495693fd12457f94e03748737d65b444ba26
DavidSoh[catalina] <mail@stdj.me>
2021-10-19 14:53:39 +0800

    add nested dictionary

--- pyBriefer.py
+++ pyBriefer.py
@@ -4,7 +4,7 @@ def brief(obj,item=4,prefix=""):
             key_list = list(obj.keys())[:item]
         else:
             key_list = list(obj.keys())
-        print(prefix+"  Count[{}]:".format(len(list(obj.keys()))))
+        print(prefix+"  count[{}]:".format(len(list(obj.keys()))))
         for key in key_list:
             print(prefix+"  - \"{}\" {}".format(key,type(obj[key])))
             value = obj[key]
@@ -14,11 +14,19 @@ def brief(obj,item=4,prefix=""):
     elif type(obj) == type(list()) or type(obj) == type(set()) :
         obj_list = list(obj)
         if len(obj_list) > item:
-            key_list = ", ".join(["\"{}\"".format(n) for n in list(obj_list)[:item]])
+            key_list = [n for n in list(obj_list)[:item]]
             end_str = ", ......"
         else:
-            key_list = ", ".join(["\"{}\"".format(n) for n in obj_list])
+            key_list = [n for n in obj_list]
             end_str = ""
-        print(prefix+"  Count[{}]: {}{}".format(len(obj_list),key_list,end_str))
+        if True in [(type(n) == type(dict())) for n in key_list]:
+            print(prefix+"  count[{}]:".format(len(obj_list)))
+            for unit_i, unit in enumerate(key_list):
+                print(prefix+"  - item[{}]".format(unit_i))
+                brief(unit,prefix="    "+prefix)
+        # if True not in [(type(n) == type(dict())) for n in key_list]:
+        else:
+            format_list = ["\"{}\"".format(n) for n in key_list]
+            print(prefix+"  count[{}]: {}{}".format(len(obj_list),", ".join(format_list),end_str))
     else:
         print(prefix+"  Content: \"{}\" {}".format(obj,type(obj)))
